58d7fbd54e9d1710dd844ff648c2e401e35ce546
AGarayev <amir.66garaev@gmail.com>
2024-05-21 10:46:13 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
-import "./App.css";
-import { TonConnectButton } from "@tonconnect/ui-react";
-import { useMainContract } from "./hooks/useMainContract";
-import { useTonConnect } from "./hooks/useTonConnect";
-import { fromNano } from "ton-core";
-import WebApp from "@twa-dev/sdk";
-
-
+import React, { useState } from 'react';
+import './App.css';
+import { TonConnectButton } from '@tonconnect/ui-react';
+import { useMainContract } from './hooks/useMainContract';
+import { useTonConnect } from './hooks/useTonConnect';
+import { fromNano } from 'ton-core';
+import WebApp from '@twa-dev/sdk';
 
 function App() {
   const {
     contract_address,
     counter_value,
-    // recent_sender,
-    // owner_address,
     contract_balance,
     sendIncrement,
     sendDeposit,
@@ -22,12 +19,18 @@ function App() {
   const { connected } = useTonConnect();
 
   const showAlert = () => {
-    WebApp.showAlert("Hey there!");
+    WebApp.showAlert('Hey there!');
+  };
+
+  const [clickCount, setClickCount] = useState(0);
+
+  const handleCircleClick = () => {
+    setClickCount((prevCount) => prevCount + 1);
   };
 
   return (
     <div>
-      <div style={{ display: "flex", justifyContent: "center" }}>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
         <TonConnectButton />
       </div>
 
@@ -35,7 +38,7 @@ function App() {
         <div className='Card'>
           <b>{WebApp.platform}</b>
           <b>Our contract Address</b>
-          <div className='Hint'>{contract_address?.slice(0, 30) + "..."}</div>
+          <div className='Hint'>{contract_address?.slice(0, 30) + '...'}</div>
           <b>Our contract Balance</b>
           {contract_balance && (
             <div className='Hint'>{fromNano(contract_balance)}</div>
@@ -44,7 +47,7 @@ function App() {
 
         <div className='Card'>
           <b>Counter Value</b>
-          <div>{counter_value ?? "Loading..."}</div>
+          <div>{counter_value ?? 'Loading...'}</div>
         </div>
 
         <a
@@ -90,6 +93,9 @@ function App() {
             Request 0.7 TON withdrawal
           </a>
         )}
+
+        <div className='Circle' onClick={handleCircleClick}></div>
+        <div>Number of clicks: {clickCount}</div>
       </div>
     </div>
   );
